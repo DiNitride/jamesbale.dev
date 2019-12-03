a8d7d7f3ac7de1c7cd01dacdf430962ba26b1758
DiNitride <j.ptb.t.ca@gmail.com>
2019-12-03 15:37:40 +0000

    Removed Strapi plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,17 +9,6 @@ module.exports = {
   /* Your site config here */
   plugins: [
     {
-      resolve: 'gatsby-source-strapi',
-      options: {
-        apiURL: 'http://localhost:1337',
-        contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
-          'project',
-          'homepage'
-        ],
-        queryLimit: 1000,
-      },
-    },
-    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
