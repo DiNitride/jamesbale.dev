bfd26fcfaa810d982b4a584e0acc8da4639ce2a8
DiNitride <j.ptb.t.ca@gmail.com>
2019-12-24 15:46:32 +0000

    Footer message

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,5 +4,10 @@ import footerStyles from "./footer.module.css"
 export default () => (
     <div className={footerStyles.footer}>
         <p>&copy; James Bale 2019</p>
+        <p>
+          This site is built using Gatsby and the Netlify CMS
+          <br/>
+          <a className={footerStyles.githubLink} href='https://www.github.com/DiNitride/jamesbale.dev'>View source code on Github</a>
+        </p>
     </div>
 )
